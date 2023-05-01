34c92cf9f8276a2c3f3d9e98d563720a1b5bdc11
astapenka.y <astapenkay@yandex.com>
2023-05-01 22:05:33 +0200

    Preparing tests: added pojo OrderList

---
+++ src/main/java/pojo/OrderList.java
@@ -0,0 +1,52 @@
+package pojo;
+
+import java.util.List;
+
+public class OrderList {
+    private Boolean success;
+    private List<Order> orders;
+    private Long total;
+    private Long totalToday;
+
+    public OrderList() {
+    }
+
+    public OrderList(Boolean success, List<Order> orders, Long total, Long totalToday) {
+        this.success = success;
+        this.orders = orders;
+        this.total = total;
+        this.totalToday = totalToday;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Long getTotalToday() {
+        return totalToday;
+    }
+
+    public void setTotalToday(Long totalToday) {
+        this.totalToday = totalToday;
+    }
+}
