664d4afdf4eb027faeba14d53b2244a998b8ab65
astapenka.y <astapenkay@yandex.com>
2023-05-01 16:26:36 +0200

    Tests: Update PostCreateOrderVerificationTest

--- src/main/java/utilities/IngredientGenerator.java
+++ src/main/java/utilities/IngredientGenerator.java
@@ -3,6 +3,7 @@ package utilities;
 import com.github.javafaker.Faker;
 import pojo.Ingredient;
 
+import java.util.HashMap;
 import java.util.Random;
 
 import static configs.RndConf.*;
@@ -13,25 +14,29 @@ public class IngredientGenerator {
     public static Ingredient generateRandomIngredient(){
 
         Faker faker = new Faker();
+        //HashMap<String, String> INGREDIENTS = new HashMap<>();
 
         String randomIngredientType = INGREDIENT_TYPES[RANDOM.nextInt(INGREDIENT_TYPES.length)];
+        String randomKey = INGREDIENTS.keySet().toArray(new String[0])[RANDOM.nextInt(INGREDIENTS.size())];
+        String randomIngredientId = INGREDIENTS.get(randomKey);
+
         String imageUrl = faker.internet().image();
         String imageMobileUrl = faker.internet().image(MIN_WIDTH_MOBILE, MAX_HEIGHT_MOBILE, false, "ingredients-mobile");
         String imageLargeUrl = faker.internet().image(MIN_WIDTH_LARGE, MAX_HEIGHT_LARGE, false, "ingredients-large");
         String ingredientName = "Cosmic_" + faker.food().ingredient();
 
         Ingredient rndIngredient = new Ingredient()
-                .setId(faker.random().hex(8))
-                .setName(ingredientName)
-                .setType(randomIngredientType)
-                .setProteins(faker.number().numberBetween(MIN_PROTEINS, MAX_PROTEINS))
-                .setFat(faker.number().numberBetween(MIN_FAT, MAX_FAT))
-                .setCarbohydrates(faker.number().numberBetween(MIN_CARBS, MAX_CARBS))
-                .setCalories(faker.number().numberBetween(MIN_CALORIES, MAX_CALORIES))
-                .setPrice(faker.number().numberBetween(MIN_PRICE, MAX_PRICE))
-                .setImage(imageUrl)
-                .setImageMobile(imageMobileUrl)
-                .setImageLarge(imageLargeUrl);
+                .setId(randomIngredientId);
+//                .setName(ingredientName)
+//                .setType(randomIngredientType)
+//                .setProteins(faker.number().numberBetween(MIN_PROTEINS, MAX_PROTEINS))
+//                .setFat(faker.number().numberBetween(MIN_FAT, MAX_FAT))
+//                .setCarbohydrates(faker.number().numberBetween(MIN_CARBS, MAX_CARBS))
+//                .setCalories(faker.number().numberBetween(MIN_CALORIES, MAX_CALORIES))
+//                .setPrice(faker.number().numberBetween(MIN_PRICE, MAX_PRICE))
+//                .setImage(imageUrl)
+//                .setImageMobile(imageMobileUrl)
+//                .setImageLarge(imageLargeUrl);
 
         return rndIngredient;
     }
--- src/test/java/orders/create/PostCreateOrderVerificationTest.java
+++ src/test/java/orders/create/PostCreateOrderVerificationTest.java
@@ -5,34 +5,42 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import io.qameta.allure.junit4.DisplayName;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 import pojo.OrderRequest;
-
-import java.io.FileNotFoundException;
+import pojo.User;
 
 import static utilities.OrderGenerator.generateRandomOrder;
 import static specs.RestAssuredSpecs.*;
+import static utilities.UserGenerator.generateRandomUser;
 
 @Feature("[Create order] POST api/orders")
 public class PostCreateOrderVerificationTest {
-    private OrderRequest orderReq;
-    private BaseSteps baseSteps;
-    @Before
-    public void setUp() throws Exception {
-        setUpSpec(requestSpec(), responseSpecCreateOrderVerified());
+    private static BaseSteps baseSteps;
+    private static String accessToken;
+    @BeforeClass
+    public static void setUp() throws Exception {
+        setUpSpec(requestSpec(), responseSpec());
+        baseSteps = new BaseSteps();
     }
 
     @Test
     @Ignore
-    @DisplayName("Create order")
-    @Description("This test verifies that an order can be created")
+    //@DisplayName("Create order | authorized")
+    @Description("This test verifies that an order can be created after authorization")
     @Severity(SeverityLevel.BLOCKER)
-    public void createNewOrder() throws FileNotFoundException {
-        baseSteps = new BaseSteps();
-        orderReq = generateRandomOrder(2);
-        //baseSteps.createNewOrder(accessToken, orderReq);
+    public void checkCreateOrderForAuthorizedUser() throws Exception {
+        // Регистрируем нового пользователя
+        User rndUser = generateRandomUser();
+        baseSteps.registrationNewUserAndVerifyResponse(rndUser);
+        // Получаем access token
+        accessToken = baseSteps.loginUserAndGetToken(rndUser);
+        // Создаем заказ
+        OrderRequest orderReq = generateRandomOrder(2);
+        baseSteps.createNewOrder(orderReq, accessToken);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        baseSteps.deleteRegisteredUser(accessToken);
     }
 }
