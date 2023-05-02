e27dd0663b80c8ab81249d22d63c07b7c0fdf7b7
astapenka.y <astapenkay@yandex.com>
2023-05-02 12:56:42 +0200

    IngredientGenerator: changed generateRandomIngredient method, used response to take id's hash

--- src/main/java/utilities/IngredientGenerator.java
+++ src/main/java/utilities/IngredientGenerator.java
@@ -1,42 +1,25 @@
 package utilities;
 
-import com.github.javafaker.Faker;
+import configs.EndPoints;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import pojo.Ingredient;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-
-import static configs.RndConf.*;
 
 public class IngredientGenerator {
 
     private static final Random RANDOM = new Random();
     public static Ingredient generateRandomIngredient(){
 
-        Faker faker = new Faker();
-        //HashMap<String, String> INGREDIENTS = new HashMap<>();
-
-        String randomIngredientType = INGREDIENT_TYPES[RANDOM.nextInt(INGREDIENT_TYPES.length)];
-        String randomKey = INGREDIENTS.keySet().toArray(new String[0])[RANDOM.nextInt(INGREDIENTS.size())];
-        String randomIngredientId = INGREDIENTS.get(randomKey);
-
-        String imageUrl = faker.internet().image();
-        String imageMobileUrl = faker.internet().image(MIN_WIDTH_MOBILE, MAX_HEIGHT_MOBILE, false, "ingredients-mobile");
-        String imageLargeUrl = faker.internet().image(MIN_WIDTH_LARGE, MAX_HEIGHT_LARGE, false, "ingredients-large");
-        String ingredientName = "Cosmic_" + faker.food().ingredient();
+        Response response = RestAssured.get(EndPoints.INGREDIENT_ENDPOINT);
+        List<String> idList = new ArrayList<>(response.jsonPath().getList("data._id", String.class));
+        String randomId = idList.get(RANDOM.nextInt(idList.size()));
 
         Ingredient rndIngredient = new Ingredient()
-                .setId(randomIngredientId);
-//                .setName(ingredientName)
-//                .setType(randomIngredientType)
-//                .setProteins(faker.number().numberBetween(MIN_PROTEINS, MAX_PROTEINS))
-//                .setFat(faker.number().numberBetween(MIN_FAT, MAX_FAT))
-//                .setCarbohydrates(faker.number().numberBetween(MIN_CARBS, MAX_CARBS))
-//                .setCalories(faker.number().numberBetween(MIN_CALORIES, MAX_CALORIES))
-//                .setPrice(faker.number().numberBetween(MIN_PRICE, MAX_PRICE))
-//                .setImage(imageUrl)
-//                .setImageMobile(imageMobileUrl)
-//                .setImageLarge(imageLargeUrl);
+                .setId(randomId);
 
         return rndIngredient;
     }
