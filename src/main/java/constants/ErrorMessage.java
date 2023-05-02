179171ff6376f67e79d42a304daff57efa98406b
astapenka.y <astapenkay@yandex.com>
2023-05-02 12:53:02 +0200

    ErrorMessage: added new error message

--- src/main/java/constants/ErrorMessage.java
+++ src/main/java/constants/ErrorMessage.java
@@ -2,9 +2,10 @@ package constants;
 
 public enum ErrorMessage {
     // User
-    CREATE_ORDER_BAD_REQUEST_ERROR_400("One or more ids provided are incorrect"),
+    CREATE_ORDER_WITHOUT_INGREDIENTS_ERROR_400("Ingredient ids must be provided"),
     USER_LOGIN_UNAUTHORIZED_ERROR_401("email or password are incorrect"),
     USER_UPDATE_UNAUTHORIZED_ERROR_401("You should be authorised"),
+    GET_ORDER_UNAUTHORIZED_ERROR_401("You should be authorised"),
     USER_CREATE_SECOND_ATTEMPT_ERROR_403("User already exists"),
     USER_CREATE_MISSING_FIELDS_ERROR_403("Email, password and name are required fields"),
     USER_UPDATE_EXISTING_EMAIL_ERROR_403("User with such email already exists");
