fc4480413e6cbe3309030c5d2ff23f0faefc2576
astapenka.y <astapenkay@yandex.com>
2023-05-07 12:33:24 +0200

    BaseSteps: moved the test steps, grouped by meaning, to other classes: RegistrationSteps, OrderSteps, UserSteps

--- src/main/java/common/BaseSteps.java
+++ src/main/java/common/BaseSteps.java
@@ -4,101 +4,25 @@ import configs.EndPoints;
 import constants.ErrorMessage;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
-import pojo.*;
+import pojo.user.User;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.*;
 import static specs.RestAssuredSpecs.*;
 
 public class BaseSteps {
-    @Step("Send registration user request and get response")
-    public Response sendRegistrationUserRequestAndGetResponse(User user) {
-
-        Response response =
-                given()
-                .when()
-                    .body(user)
-                    .post(EndPoints.REGISTRATION_ENDPOINT)
-                .then()
-                    .spec(failureUserResponse())
-                    .extract().response();
-
-        return response;
-    }
-
     @Step("Send user login request and get response")
     public Response sendLoginUserRequestAndGetResponse(User user) {
-
-        Response response =
-                given()
+        return given()
                 .when()
                     .body(user)
                     .post(EndPoints.LOGIN_ENDPOINT)
                 .then()
                     .spec(failureUserResponse())
                     .extract().response();
-
-        return response;
     }
 
-    @Step("Send user update request and get response")
-    public Response sendPatchUserRequestAndGetResponse(User user, String newEmail, String newName, String accessToken) {
-
-        User updatedUser = new User();
-        updatedUser.setEmail(newEmail);
-        updatedUser.setName(newName);
-
-        Response response =
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .body(updatedUser)
-                    .patch(EndPoints.USER_ENDPOINT)
-                .then()
-                    .spec(failureUserResponse())
-                .extract().response();
-
-        return response;
-    }
-
-    @Step("Send order create request and get response")
-    public Response sendPostCreateOrderAndGetResponse(OrderRequest orderReqJson, String accessToken) {
-        Response response =
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .body(orderReqJson)
-                    .post(EndPoints.ORDER_ENDPOINT)
-                .then()
-                    .extract().response();
-
-        return response;
-    }
-
-    @Step("Send user update request and get response")
-    public Response sendGetOrderListAndGetResponse(String accessToken) {
-        Response response =
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .get(EndPoints.ORDER_ENDPOINT)
-                .then()
-                    .extract().response();
-        return response;
-    }
-
-    @Step("Registration new user")
-    public void registrationNewUserAndVerifyResponse(User user) {
-                given()
-                .when()
-                    .body(user)
-                    .post(EndPoints.REGISTRATION_ENDPOINT)
-                .then()
-                    .spec(successUserResponse(user.getEmail(), user.getName()))
-                    .extract().response().as(AuthResponse.class);
-    }
-
-    @Step("User authentication")
+    @Step("User login")
     public void loginUser(User user) {
                 given()
                 .when()
@@ -110,91 +34,13 @@ public class BaseSteps {
 
     @Step("User login and get access token")
     public String loginUserAndGetToken(User user) {
-
-        String accessToken =
-                given()
+        return given()
                 .when()
                     .body(user)
                     .post(EndPoints.LOGIN_ENDPOINT)
                 .then()
                     .spec(successUserResponse(user.getEmail(), user.getName()))
                     .extract().path("accessToken");
-
-        return accessToken;
-    }
-
-    @Step("Get user data and verify response | authorized")
-    public void getUserAndVerifyResponse(User user, String accessToken) {
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .get(EndPoints.USER_ENDPOINT)
-                .then()
-                    .spec(successGetUserResponse(user.getEmail(), user.getName()));
-    }
-
-    @Step("Check user is not found")
-    public void checkUserNotFound(String accessToken) {
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .get(EndPoints.USER_ENDPOINT)
-                .then()
-                    .spec(userNotFoundResponse());
-    }
-
-    @Step("Change user email and name to new values")
-    public void updateUserEmailAndName(User user, String newEmail, String newName, String accessToken) {
-                user.setEmail(newEmail);
-                user.setName(newName);
-
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .body(user)
-                    .patch(EndPoints.USER_ENDPOINT)
-                .then()
-                    .spec(successGetUserResponse(user.getEmail(), user.getName()));
-    }
-    @Step("Delete user")
-    public void deleteUser(String accessToken) {
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .delete(EndPoints.USER_ENDPOINT)
-                .then()
-                    .spec(successDeleteRegisteredUserResponse());
-    }
-
-    @Step("Delete user")
-    public void deleteRegisteredUser(String accessToken) {
-        deleteUser(accessToken);
-        checkUserNotFound(accessToken);
-    }
-
-
-    @Step("Create a new order")
-    public void createNewOrderAndVerifyResponse(OrderRequest orderReqJson, String accessToken) {
-
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .body(orderReqJson)
-                    .post(EndPoints.ORDER_ENDPOINT)
-                .then()
-                    .spec(successOrderResponse())
-                    .extract().response().as(OrderResponse.class);
-    }
-
-    @Step("Get order list")
-    public void getOrderListAndVerifyResponse( String accessToken) {
-                given()
-                    .header("Authorization", accessToken)
-                .when()
-                    .get(EndPoints.ORDER_ENDPOINT)
-                .then()
-                    .spec(successOrderListResponse())
-                .extract().response().as(OrderList.class);
     }
 
     @Step ("Check response for invalid parameters")
