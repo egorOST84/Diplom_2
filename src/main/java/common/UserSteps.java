98fd5f7a9770c19c4adc297d8fbe1be80d1eb28d
astapenka.y <astapenkay@yandex.com>
2023-05-07 12:30:12 +0200

    Preparing tests: moved 2 steps to UserSteps from BaseSteps

--- src/main/java/common/UserSteps.java
+++ src/main/java/common/UserSteps.java
@@ -2,6 +2,7 @@ package common;
 
 import configs.EndPoints;
 import io.qameta.allure.Step;
+import io.restassured.response.Response;
 import pojo.user.User;
 
 import static io.restassured.RestAssured.given;
@@ -26,6 +27,37 @@ public class UserSteps {
                 .get(EndPoints.USER_ENDPOINT)
                 .then()
                 .spec(userNotFoundResponse());
+    }
+
+    @Step("Send user update request and get response")
+    public Response sendPatchUserRequestAndGetResponse(User user, String newEmail, String newName, String accessToken) {
+
+        User updatedUser = new User();
+        updatedUser.setEmail(newEmail);
+        updatedUser.setName(newName);
+
+        return given()
+                .header("Authorization", accessToken)
+                .when()
+                .body(updatedUser)
+                .patch(EndPoints.USER_ENDPOINT)
+                .then()
+                .spec(failureUserResponse())
+                .extract().response();
+    }
+
+    @Step("Change user email and name to new values")
+    public void updateUserEmailAndName(User user, String newEmail, String newName, String accessToken) {
+        user.setEmail(newEmail);
+        user.setName(newName);
+
+        given()
+                .header("Authorization", accessToken)
+                .when()
+                .body(user)
+                .patch(EndPoints.USER_ENDPOINT)
+                .then()
+                .spec(successGetUserResponse(user.getEmail(), user.getName()));
     }
 
     @Step("Delete user")
